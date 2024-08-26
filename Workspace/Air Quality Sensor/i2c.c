fcbb44d9af79d5649f2ad01791d5581114bf0452
Nicholas Mascioli <nicholas.mascioli@electronics4all.ca>
2024-08-26 11:46:29 -0400

    Reworked file structure

--- Workspace/Air Quality Sensor/i2c.c
+++ Workspace/Air Quality Sensor/i2c.c
@@ -4,7 +4,36 @@
  *  Created on: Aug. 23, 2024
  *      Author: NicholasMascioli
  */
+#include <stdint.h>
+#include <stddef.h>
+#include <unistd.h>
+#include <ti/drivers/I2C.h>
+#include "i2c.h"
+
+#define placer 0
+
+static I2C_Handle i2c = NULL;
+
+static I2C_Handle i2cOpen(void);
+
+bool i2c_write(uint8_t address, uint8_t *txBytes,  uint8_t count){
+
+}
+
+bool i2c_read(uint8_t address, uint8_t *txBytes,  uint8_t count){
+
+}
+
+bool i2c_init(void){
+    I2C_init();
+    I2C_Params i2cParams;
+    i2cParams.bitRate = I2C_400kHz;
 
 
+    i2cParams.transferMode = I2C_MODE_BLOCKING;
+    i2cParams.transferCallbackFxn = NULL;
+    i2cParams.custom = NULL;
+    i2c = I2C_open(PLACER, &i2cParams);
 
-
+    return i2c;
+}
---
+++ Workspace/i2ctmp_CC26X2R1_LAUNCHXL_tirtos7_ticlang/eeprom.h
@@ -0,0 +1,15 @@
+/*
+ * eeprom.h
+ *
+ *  Created on: Aug. 23, 2024
+ *      Author: NicholasMascioli
+ */
+
+#ifndef EEPROM_H_
+#define EEPROM_H_
+
+
+
+
+
+#endif /* EEPROM_H_ */
