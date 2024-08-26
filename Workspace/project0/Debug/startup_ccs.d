2bdbcaabaee2848915afff586a477c8764e99743
Nicholas Mascioli <nicholas.mascioli@electronics4all.ca>
2024-08-26 15:31:22 -0400

    Cleaned up old files

---
+++ Workspace/project0/Debug/startup_ccs.d
@@ -0,0 +1,42 @@
+# FIXED
+
+startup_ccs.obj: ../startup_ccs.c
+startup_ccs.obj: C:/ti/include/stdint.h
+startup_ccs.obj: C:/ti/include/_ti_config.h
+startup_ccs.obj: C:/ti/include/linkage.h
+startup_ccs.obj: C:/ti/include/_stdint40.h
+startup_ccs.obj: C:/ti/include/sys/stdint.h
+startup_ccs.obj: C:/ti/include/sys/cdefs.h
+startup_ccs.obj: C:/ti/include/sys/_types.h
+startup_ccs.obj: C:/ti/include/machine/_types.h
+startup_ccs.obj: C:/ti/include/machine/_stdint.h
+startup_ccs.obj: C:/ti/include/sys/_stdint.h
+startup_ccs.obj: C:/ti/TivaWare_C_Series-2.2.0.295/inc/hw_nvic.h
+startup_ccs.obj: C:/ti/TivaWare_C_Series-2.2.0.295/inc/hw_types.h
+
+../startup_ccs.c:
+
+C:/ti/include/stdint.h:
+
+C:/ti/include/_ti_config.h:
+
+C:/ti/include/linkage.h:
+
+C:/ti/include/_stdint40.h:
+
+C:/ti/include/sys/stdint.h:
+
+C:/ti/include/sys/cdefs.h:
+
+C:/ti/include/sys/_types.h:
+
+C:/ti/include/machine/_types.h:
+
+C:/ti/include/machine/_stdint.h:
+
+C:/ti/include/sys/_stdint.h:
+
+C:/ti/TivaWare_C_Series-2.2.0.295/inc/hw_nvic.h:
+
+C:/ti/TivaWare_C_Series-2.2.0.295/inc/hw_types.h:
+
