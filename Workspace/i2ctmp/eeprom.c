40a7104357c12ca7867fd8f688494ae803aba339
Nicholas Mascioli <nicholas.mascioli@electronics4all.ca>
2024-08-26 15:29:01 -0400

    Added eeprom methods

--- Workspace/i2ctmp/eeprom.c
+++ Workspace/i2ctmp/eeprom.c
@@ -5,6 +5,48 @@
  *      Author: NicholasMascioli
  */
 
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <ti/drivers/GPIO.h>
+#include <ti/display/Display.h>
 
+#include "eeprom.h"
+#include "i2c.h"
+#include <ti/sysbios/knl/Task.h>
+
+
+bool eeprom_write_byte          (uint32_t address, uint8_t byte ){
+    return false;
+}
+
+bool eeprom_write_page          (uint16_t page, uint8_t address, uint8_t byte[], uint8_t size ){
+    return false;
+}
+
+bool eeprom_read_current        (uint8_t *byte ){
+    return false;
+}
+
+bool eeprom_read_byte           (uint32_t address, uint8_t *byte ){
+    return false;
+}
+
+bool eeprom_read_sequential     (uint32_t address, uint8_t *byte, uint32_t size ){
+    return false;
+}
+
+bool eeprom_clear_byte          (uint32_t address){
+    return false;
+}
+
+bool eeprom_clear_page          (uint16_t page, uint8_t address, uint8_t size ){
+    return false;
+}
+
+bool eeprom_clear_all           (void){
+    return false;
+}
 
 
--- Workspace/i2ctmp/eeprom.h
+++ Workspace/i2ctmp/eeprom.h
@@ -11,8 +11,21 @@
 #include "ti_drivers_config.h"
 
 
-#define TYPE_ID 0xA
+#define EEPROM_TYPE_ID          0xA
+#define EEPROM_ADR_MASK         0x40000
 
+#define EEPROM_WRITE            0x0
+#define EEPROM_READ             0x1
 
+bool eeprom_write_byte          (uint32_t address, uint8_t byte );
+bool eeprom_write_page          (uint16_t page, uint8_t address, uint8_t byte[], uint8_t size );
+
+bool eeprom_read_current        (uint8_t *byte );
+bool eeprom_read_byte           (uint32_t address, uint8_t *byte );
+bool eeprom_read_sequential     (uint32_t address, uint8_t *byte, uint32_t size );
+
+bool eeprom_clear_byte          (uint32_t address);
+bool eeprom_clear_page          (uint16_t page, uint8_t address, uint8_t size );
+bool eeprom_clear_all           (void);
 
 #endif /* EEPROM_H_ */
