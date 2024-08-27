817919ad3c897efd85c40b4743e74ad60ab333e3
Nicholas Mascioli <nicholas.mascioli@electronics4all.ca>
2024-08-27 11:23:17 -0400

    Added a few eeprom methods

--- Workspace/i2ctmp/eeprom.c
+++ Workspace/i2ctmp/eeprom.c
@@ -17,35 +17,76 @@
 #include <ti/sysbios/knl/Task.h>
 
 
-bool eeprom_write_byte          (uint32_t address, uint8_t byte ){
+bool eeprom_write_byte(uint32_t address, uint8_t byte )
+{
+    bool flag;
+    uint8_t *tx_data = (uint8_t*)malloc(3 * sizeof(uint8_t));
+
+    address = address & EEPROM_ADR_MASK;
+
+    uint8_t adr = EEPROM_TYPE_ID | ((address >> 16) << 1) | EEPROM_WRITE;
+
+    tx_data[0]  = (address >> 8)    & EEPROM_BYTE_MASK;
+    tx_data[1]  = address           & EEPROM_BYTE_MASK;
+    tx_data[2]  = byte;
+
+    flag = i2c_write(adr, tx_data, 3);
+
+    free(tx_data);
+
+    return flag;
+}
+
+bool eeprom_write_page(uint16_t page, uint8_t address, uint8_t byte[], uint8_t size )
+{
+    bool flag;
+    uint8_t *tx_data = (uint8_t*)malloc((size+2) * sizeof(uint8_t));
+
+    address = address & EEPROM_ADR_MASK;
+
+    uint8_t adr = EEPROM_TYPE_ID | ((address >> 16) << 1) | EEPROM_WRITE;
+
+    tx_data[0]  = (address >> 8)    & EEPROM_BYTE_MASK;
+    tx_data[1]  = address           & EEPROM_BYTE_MASK;
+
+    for(int i=0;i<size;i++){
+        tx_data[i+2] = byte[i];
+    }
+
+    flag = i2c_write(adr, tx_data, size+2);
+
+    free(tx_data);
+
+    return flag;
+}
+
+bool eeprom_read_current(uint8_t *byte )
+{
     return false;
 }
 
-bool eeprom_write_page          (uint16_t page, uint8_t address, uint8_t byte[], uint8_t size ){
+bool eeprom_read_byte(uint32_t address, uint8_t *byte )
+{
     return false;
 }
 
-bool eeprom_read_current        (uint8_t *byte ){
+bool eeprom_read_sequential(uint32_t address, uint8_t *byte, uint32_t size )
+{
     return false;
 }
 
-bool eeprom_read_byte           (uint32_t address, uint8_t *byte ){
+bool eeprom_clear_byte(uint32_t address)
+{
     return false;
 }
 
-bool eeprom_read_sequential     (uint32_t address, uint8_t *byte, uint32_t size ){
+bool eeprom_clear_page(uint16_t page, uint8_t address, uint8_t size )
+{
     return false;
 }
 
-bool eeprom_clear_byte          (uint32_t address){
-    return false;
-}
-
-bool eeprom_clear_page          (uint16_t page, uint8_t address, uint8_t size ){
-    return false;
-}
-
-bool eeprom_clear_all           (void){
+bool eeprom_clear_all(void)
+{
     return false;
 }
 
--- Workspace/i2ctmp/eeprom.h
+++ Workspace/i2ctmp/eeprom.h
@@ -11,21 +11,22 @@
 #include "ti_drivers_config.h"
 
 
-#define EEPROM_TYPE_ID          0xA
-#define EEPROM_ADR_MASK         0x40000
+#define EEPROM_TYPE_ID          0xA0
+#define EEPROM_ADR_MASK         0x3FFFF
+#define EEPROM_BYTE_MASK        0xFF
 
-#define EEPROM_WRITE            0x0
-#define EEPROM_READ             0x1
+#define EEPROM_WRITE            0x00
+#define EEPROM_READ             0x01
 
-bool eeprom_write_byte          (uint32_t address, uint8_t byte );
-bool eeprom_write_page          (uint16_t page, uint8_t address, uint8_t byte[], uint8_t size );
+bool eeprom_write_byte          (uint32_t address, uint8_t byte);
+bool eeprom_write_page          (uint16_t page, uint8_t address, uint8_t byte[], uint8_t size);
 
 bool eeprom_read_current        (uint8_t *byte );
-bool eeprom_read_byte           (uint32_t address, uint8_t *byte );
-bool eeprom_read_sequential     (uint32_t address, uint8_t *byte, uint32_t size );
+bool eeprom_read_byte           (uint32_t address, uint8_t *byte);
+bool eeprom_read_sequential     (uint32_t address, uint8_t *byte, uint32_t size);
 
 bool eeprom_clear_byte          (uint32_t address);
-bool eeprom_clear_page          (uint16_t page, uint8_t address, uint8_t size );
+bool eeprom_clear_page          (uint16_t page, uint8_t address, uint8_t size);
 bool eeprom_clear_all           (void);
 
 
